8ce348f6030f8f23946e838d386b4bb3a17b878c
andrewmanq <Everest42>
2019-02-21 13:11:50 -0500

    more work on project 1

--- homework01/travelling_salesman.py
+++ homework01/travelling_salesman.py
@@ -1,24 +1,15 @@
-from tools.aima.search import Problem, hill_climbing, simulated_annealing, \
-    exp_schedule, genetic_search
+from tools.aima.search import *
 from random import randrange
 import math
 import time
 
+if(__name__=="__main__"):
 
-class TravellingSalesman(Problem):
-    """
-    State: x value for the abs function variant f(x)
-    Move: a new x value delta steps from the current x (in both directions)
-    """
+    numCities = 4
 
-    def __init__(self, cities=2):
-        self.cities = cities
+    cityDict = dict(StartingNode=dict(EndingNode=10000))
+    cityDict.pop(thirdCity=dict(cityDict=10, EndingNode=20))
 
-    def actions(self, state):
-        return [state + self.delta, state - self.delta]
+    theMap = UndirectedGraph(cityDict)
 
-    def result(self, stateIgnored, x):
-        return x
-
-    def value(self, x):
-        return self.maximum / 2 - math.fabs(self.maximum / 2 - x)
+    theProblem = GraphProblem()
